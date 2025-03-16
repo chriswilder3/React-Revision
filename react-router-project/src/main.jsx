2680e1303ae26c2b6446c061d07bd1d52b6a09b5
chriswilder3 <chriswilderdelta19@gmail.com>
2025-03-16 17:32:56 +0530

    added layout and basic router

--- react-router-project/src/main.jsx
+++ react-router-project/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout';
+import Home from './components/home/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+     < Route path='/' element={ <Layout />}>
+        <Route path='' element={ <Home/>} />
+     </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
